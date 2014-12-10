ca4f401bbd23c11f50592cf325ec0cab87c5a85a
Keiichi Watanabe <udon.watanabe@gmail.com>
2014-12-11 03:31:49 +0900

    update travis config

--- travis-ci.sh
+++ travis-ci.sh
@@ -1,25 +1,18 @@
-# OPAM version to install
-export OPAM_VERSION=1.1.1
 # OPAM packages needed to build tests
 export OPAM_PACKAGES='ocamlfind menhir'
 
 # install ocaml from apt
+echo "yes" | sudo add-apt-repository ppa:avsm/ocaml41+opam11
 sudo apt-get update -qq
-sudo apt-get install -qq ocaml
+sudo apt-get install -qq ocaml ocaml-native-compilers opam
 
-# install opam
-curl -L https://github.com/OCamlPro/opam/archive/${OPAM_VERSION}.tar.gz | tar xz -C /tmp
-pushd /tmp/opam-${OPAM_VERSION}
-./configure
-make
-sudo make install
-opam init
-eval `opam config -env`
-popd
+echo OCaml version
+ocaml -version
 
 # install packages from opam
+opam init
+opam update
 opam install -q -y ${OPAM_PACKAGES}
 
 # compile & run tests
-git submodule update --init extlib/
 make test
