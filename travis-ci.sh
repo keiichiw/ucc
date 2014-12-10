5b1fec0554b5ee69aaa03998f06ffb9732c7dd45
Keiichi Watanabe <udon.watanabe@gmail.com>
2014-12-11 03:52:39 +0900

    fix travis config

--- travis-ci.sh
+++ travis-ci.sh
@@ -6,12 +6,11 @@ echo "yes" | sudo add-apt-repository ppa:avsm/ocaml41+opam11
 sudo apt-get update -qq
 sudo apt-get install -qq ocaml ocaml-native-compilers opam
 
-echo OCaml version
 ocaml -version
 
 # install packages from opam
-opam init
-opam update
+opam init -y
+opam update -y
 opam install -q -y ${OPAM_PACKAGES}
 
 # compile & run tests
