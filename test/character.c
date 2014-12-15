abb204ba9797831e057ea7fc2531ccdefeed0a5f
Wataru Inariba <oinari17@gmail.com>
2014-12-15 22:30:55 +0900

    support character constant

---
+++ test/character.c
@@ -0,0 +1,33 @@
+/*
+32
+65
+97
+48
+47
+126
+0
+10
+39
+92
+27
+27
+25
+51
+*/
+
+int main() {
+  print_int(' ');
+  print_int('A');
+  print_int('a');
+  print_int('0');
+  print_int('/');
+  print_int('~');
+  print_int('\0');
+  print_int('\n');
+  print_int('\'');
+  print_int('\\');
+  print_int('\033');
+  print_int('\x1b');
+  print_int('z' - 'a');
+  print_int('0' + 3);
+}
