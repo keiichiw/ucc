a0b869bd3fbe75d00b3095b2491766ad15144887
Keiichi Watanabe <udon.watanabe@gmail.com>
2014-12-18 02:58:09 +0900

    add test case for comparison

---
+++ test/comp.c
@@ -0,0 +1,73 @@
+/*
+1
+1
+0
+0
+0
+1
+0
+1
+1
+1
+0
+0
+1
+1
+0
+0
+0
+0
+1
+1
+0
+0
+1
+1
+0
+0
+1
+1
+0
+1
+0
+1
+*/
+
+int main () {
+  print_int(1< 2);
+  print_int(1<=2);
+  print_int(1> 2);
+  print_int(1>=2);
+  print_int(1< 1);
+  print_int(1<=1);
+  print_int(1> 1);
+  print_int(1>=1);
+
+  print_int(-1< 2);
+  print_int(-1<=2);
+  print_int(-1> 2);
+  print_int(-1>=2);
+  print_int(-1< 1);
+  print_int(-1<=1);
+  print_int(-1> 1);
+  print_int(-1>=1);
+
+  print_int(1< -2);
+  print_int(1<=-2);
+  print_int(1> -2);
+  print_int(1>=-2);
+  print_int(1< -1);
+  print_int(1<=-1);
+  print_int(1> -1);
+  print_int(1>=-1);
+
+  print_int(-1< -2);
+  print_int(-1<=-2);
+  print_int(-1> -2);
+  print_int(-1>=-2);
+  print_int(-1< -1);
+  print_int(-1<=-1);
+  print_int(-1> -1);
+  print_int(-1>=-1);
+  return 0;
+}
