c589ba3e99e6f964fa93660c534ec89665252d40
Keiichi Watanabe <udon.watanabe@gmail.com>
2015-01-23 06:48:37 +0900

    update travis-ci.sh

--- travis-ci.sh
+++ travis-ci.sh
@@ -1,17 +1,9 @@
-# OPAM packages needed to build tests
-export OPAM_PACKAGES='ocamlfind'
-
 # install ocaml from apt
 yes yes | sudo add-apt-repository ppa:avsm/ppa
 sudo apt-get update -qq
-sudo apt-get install -qq ocaml ocaml-native-compilers opam menhir
+sudo apt-get install -qq ocaml ocaml-native-compilers
 
 ocaml -version
-
-# install packages from opam
-opam init -y
-opam update -y
-opam install -q -y ${OPAM_PACKAGES}
 
 # compile & run tests
 make test
