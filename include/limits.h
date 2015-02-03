14b27866c790da1e612e8ad44b4e2c7e982bd594
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2015-02-03 20:08:08 +0900

    add UINT_MAX and ULONG_MAX macros

--- include/limits.h
+++ include/limits.h
@@ -4,7 +4,9 @@
 #define CHAR_BIT   32
 #define INT_MIN  (-2147483648)
 #define INT_MAX    2147483647
+#define UINT_MAX (0xffffffffu)
 #define LONG_MIN   INT_MIN
 #define LONG_MAX   INT_MAX
+#define ULONG_MAX  UINT_MAX
 
 #endif  /* limits.h */
