799c8e2193c5162e256dca7adf1d6f56fb366de3
Wataru Inariba <oinari17@gmail.com>
2015-02-06 21:27:59 +0900

    fix stdarg.h

--- include/stdarg.h
+++ include/stdarg.h
@@ -1,9 +1,9 @@
 #ifndef _STDARG_H
 #define _STDARG_H
 
-#define va_arg(ap, type)   *(type*)(ap+=sizeof(type))
-#define va_start(ap, arg)  (ap)=&(arg)
+#define va_arg(ap, type)   (*((type *)(ap = (type *)ap + 1) - 1))
+#define va_start(ap, arg)  (ap = &(arg) + 1)
 #define va_end(ap)         ((void) 0)
-typedef char *va_list;
+typedef void *va_list;
 
 #endif  /* stdarg.h */
