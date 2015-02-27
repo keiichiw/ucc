0ffe6faa607cd03c7c47ade39db8b252f3d0ece0
Keiichi Watanabe <udon.watanabe@gmail.com>
2015-02-27 21:22:34 +0900

    update limits.h

--- include/limits.h
+++ include/limits.h
@@ -1,12 +1,20 @@
 #ifndef _UCC_LIMITS_H
 #define _UCC_LIMITS_H
 
-#define CHAR_BIT   32
-#define INT_MIN  (-2147483648)
-#define INT_MAX    2147483647
-#define UINT_MAX (0xffffffffu)
-#define LONG_MIN   INT_MIN
-#define LONG_MAX   INT_MAX
-#define ULONG_MAX  UINT_MAX
+#define CHAR_BIT  8
+#define SCHAR_MAX 127
+#define SCHAR_MIN (-128)
+#define UCHAR_MAX 255
+#define CHAR_MAX  127
+#define CHAR_MIN  (-128)
+#define SHRT_MAX  32767
+#define SHRT_MIN  (-32768)
+#define USHRT_MAX 65535
+#define INT_MAX   2147483647
+#define INT_MIN   (-2147483648)
+#define UINT_MAX  4294967295
+#define LONG_MAX  INT_MAX
+#define LONG_MIN  INT_MIN
+#define ULONG_MAX UINT_MAX
 
 #endif  /* limits.h */
--- test/limits.c
+++ test/limits.c
@@ -1,18 +1,38 @@
 /*
-32
--2147483648
+8
+127
+-128
+255
+127
+-128
+32767
+-32768
+65535
 2147483647
 -2147483648
+4294967295
 2147483647
+-2147483648
+4294967295
 */
 
 #include <limits.h>
 #include "test.h"
 int main () {
   printf("%d\n", CHAR_BIT);
-  printf("%d\n",  INT_MIN);
+  printf("%d\n", SCHAR_MAX);
+  printf("%d\n", SCHAR_MIN);
+  printf("%d\n", UCHAR_MAX);
+  printf("%d\n", CHAR_MAX);
+  printf("%d\n", CHAR_MIN);
+  printf("%d\n", SHRT_MAX);
+  printf("%d\n", SHRT_MIN);
+  printf("%d\n", USHRT_MAX);
   printf("%d\n",  INT_MAX);
-  printf("%d\n", LONG_MIN);
-  printf("%d\n", LONG_MAX);
+  printf("%d\n",  INT_MIN);
+  printf("%u\n",  UINT_MAX);
+  printf("%ld\n", LONG_MAX);
+  printf("%ld\n", LONG_MIN);
+  printf("%lu\n", ULONG_MAX);
   return 0;
 }
